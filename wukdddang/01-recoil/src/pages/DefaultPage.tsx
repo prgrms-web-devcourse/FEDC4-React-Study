5969eea61496f3c84b3a2afc5f6bac3dc506d6d9
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-08-23 10:51:14 +0900

    Feat: DefaultPage 프레젠테이션 컴포넌트 구현 - 헤더 높이를 계산한 BackDrop 컴포넌트 - Outlet을 통해 자식 Route 컴포넌트를 렌더링 (Placeholder 개념)

---
+++ wukdddang/01-recoil/src/pages/DefaultPage.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Outlet } from "react-router";
+import { styled } from "styled-components";
+
+const View = styled.main`
+  width: 100%;
+  max-width: 1024px;
+  margin: 0 auto;
+  height: 100%;
+  min-height: calc(100vh - 72px);
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  background-color: #fff;
+`;
+
+const BackDrop = styled.div`
+  width: 100%;
+  margin-top: 72px;
+  min-height: calc(100vh - 72px);
+`;
+
+export default function DefaultPage() {
+  return (
+    <BackDrop>
+      <View>
+        <Outlet></Outlet>
+      </View>
+    </BackDrop>
+  );
+}
