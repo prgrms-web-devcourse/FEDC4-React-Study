f30d06e12b218e904b7c54b1d5f860f924930776
colorkite10 <colorkite10@gmail.com>
2023-08-23 23:48:03 +0900

    Feat: Square에 value props 넘겨주기

--- chaeyeon/tic-tac-toe/src/App.js
+++ chaeyeon/tic-tac-toe/src/App.js
@@ -1,20 +1,27 @@
-export default function Square() {
+// Passing data through props
+// 사용자가 사각형을 클릭하면 X로 변경하게 하기
+
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
+export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   );
