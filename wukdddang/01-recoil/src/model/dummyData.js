d63496405f1e4119d4e0db8c657f4cfdcfe89e9c
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-08-23 10:34:06 +0900

    Feat: 더미데이터 추가

---
+++ wukdddang/01-recoil/src/model/dummyData.js
@@ -0,0 +1,39 @@
+const dummyData =[
+  {
+    id: 1,
+    price: 130000,
+    title: "[리코일] Recoil made in USA",
+    description: "리코일은 정말 좋은 상태관리 라이브러리에요",
+  },
+  {
+    id: 2,
+    price: 90000,
+    title: "[리액트] React is good",
+    description: "리액트가 솔직히 짱이죠",
+  },
+  {
+    id: 3,
+    price: 1000,
+    title: "[잡캐] 이것저것 좋아하는 잡캐",
+    description: "이것저것 많이 해보는게 틀린건 아니잖아요?",
+  },
+  {
+    id: 4,
+    price: 1390000,
+    title: "[헨리] Henry made in Korea",
+    description: "여러분의 개발자라이프 응원할게요!",
+  },
+  {
+    id: 5,
+    price: 90000,
+    title: "[리액트] React is good",
+    description: "리액트가 솔직히 짱이죠",
+  },
+  {
+    id: 6,
+    price: 1390000,
+    title: "[헨리] Henry made in Korea",
+    description: "여러분의 개발자라이프 응원할게요!",
+  },
+]
+export default dummyData
