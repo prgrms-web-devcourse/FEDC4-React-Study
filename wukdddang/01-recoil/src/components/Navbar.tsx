6088423ca94e2836960228b6509c3b28985d047c
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-08-23 10:45:31 +0900

    Feat: Navbar 컴포넌트 추가 - Link 컴포넌트로 라우팅 기능 구현

---
+++ wukdddang/01-recoil/src/components/Navbar.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { styled } from "styled-components";
+import { Link } from "react-router-dom";
+import logo from "/vite.svg";
+
+const HeaderWrapper = styled.div`
+  width: 100%;
+  height: 72px;
+  border-bottom: 1px solid #ccc;
+  position: fixed;
+  top: 0;
+  z-index: 9999;
+  background-color: #fff;
+`;
+
+const Header = styled.header`
+  width: 100%;
+  height: 100%;
+  max-width: 1024px;
+  padding: 0 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+`;
+
+const Nav = styled.nav`
+  max-width: 1024px;
+  display: flex;
+  justify-content: right;
+  gap: 36px;
+  align-items: center;
+
+  & > .url-route {
+    font-weight: bold;
+    cursor: pointer;
+  }
+`;
+
+export default function Navbar() {
+  return (
+    <HeaderWrapper>
+      <Header>
+        <Link to={"/"} aria-label="홈으로 이동">
+          <img src={logo} alt="logo" width="48" height="48" />
+        </Link>
+        <Nav>
+          <Link className="url-route" to={"/"} aria-label="홈으로 이동">
+            홈
+          </Link>
+          <Link className="url-route" to={"/cart"} aria-label="장바구니로 이동">
+            장바구니
+          </Link>
+        </Nav>
+      </Header>
+    </HeaderWrapper>
+  );
+}
