3cce62dcf16bf26c69efdcb7afc512206a05a0a3
colorkite10 <colorkite10@gmail.com>
2023-08-24 22:56:09 +0900

    Feat: 부모컴포넌트에 자식컴포넌트 상태 전달, OX번갈아가며 클릭 가능

--- chaeyeon/tic-tac-toe/src/App.js
+++ chaeyeon/tic-tac-toe/src/App.js
@@ -1,41 +1,55 @@
 import { useState } from "react";
-// Making an interactive component
-// 누르면 X로 채우기
 
-// 리액트는 컴포넌트에서 호출하여 항목을 기억할 수 있는 useState를 제공함.
-
-function Square() {
-  const [value, setValue] = useState(null);
-  //value는 값을 저장, setValue는 값을 변경하는 데 사용되는 함수
-  //null은 useState의 상태변수의 초기값으로 사용된다.
-
-  function handleClick() {
-    setValue("X");
-  }
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  //['O', null, 'X', 'X', 'X', 'O', 'O', null, null]
+
+  //closures
+  function handleClick(i) {
+    //이미 squares[i]에 값이 할당되어 있다면 returnning early
+    //클릭한 네모칸을 중복 클릭하는 것을 막는다.
+    if (squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    // slice()를 통해 squares배열의 복사본을 만든다.
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+    // set-: 컴포넌트의 상태가 변경되었다는 것을 알린다.
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
