7e6401f6b511ff247e15b23001f65349e8953afd
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-08-24 09:25:08 +0900

    Feat: 코드 스플리팅 적용 (임시)

--- wukdddang/01-recoil/vite.config.ts
+++ wukdddang/01-recoil/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom", "recoil"],
+        },
+      },
+    },
+  },
+});
