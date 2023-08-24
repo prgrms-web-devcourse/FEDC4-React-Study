baab691b071c919fec024f6da7ba32970dd256f1
colorkite10 <colorkite10@gmail.com>
2023-08-24 12:29:36 +0900

    Feat: 칸 누르면 X로 변하도록 useState 이용하기

--- chaeyeon/tic-tac-toe/src/App.js
+++ chaeyeon/tic-tac-toe/src/App.js
@@ -1,27 +1,41 @@
-// Passing data through props
-// 사용자가 사각형을 클릭하면 X로 변경하게 하기
+import { useState } from "react";
+// Making an interactive component
+// 누르면 X로 채우기
 
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+// 리액트는 컴포넌트에서 호출하여 항목을 기억할 수 있는 useState를 제공함.
+
+function Square() {
+  const [value, setValue] = useState(null);
+  //value는 값을 저장, setValue는 값을 변경하는 데 사용되는 함수
+  //null은 useState의 상태변수의 초기값으로 사용된다.
+
+  function handleClick() {
+    setValue("X");
+  }
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
