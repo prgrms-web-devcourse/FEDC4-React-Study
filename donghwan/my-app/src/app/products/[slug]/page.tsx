cd0a4cd83b14f1717362881618e0b55ad24260e5
Dongja <jdh981118@naver.com>
2023-08-25 17:18:27 +0900

    Routing: SSG (dynamic routing)

--- donghwan/my-app/src/app/products/[slug]/page.tsx
+++ donghwan/my-app/src/app/products/[slug]/page.tsx
@@ -7,3 +7,10 @@ type Props = {
 export default function Pants({ params }: Props) {
   return <h1>{params.slug} 소개 페이지입니다!</h1>;
 }
+
+export function generateStaticParams() {
+  const products = ['pants', 'shirt'];
+  return products.map((product) => ({
+    slug: product,
+  }));
+}
