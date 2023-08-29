d6d01ee0af1fd6124c790604b1f023ba85c6b46c
Dongja <jdh981118@naver.com>
2023-08-29 15:36:17 +0900

    feat: SEO - MeatData

--- donghwan/my-app/src/app/products/layout.tsx
+++ donghwan/my-app/src/app/products/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
 import styles from './layout.module.css';
+
+export const metadata: Metadata = {
+  title: '멋진 제품 사이트 | 전체 제품 확인',
+  description: '멋진 제품을 확인해 보세요.',
+};
 
 export default function ProductsLayout({
   children,
