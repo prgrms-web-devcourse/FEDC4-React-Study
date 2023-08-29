c67178b00cc0b0b88769a0c32872f4c4aa31e58c
Dongja <jdh981118@naver.com>
2023-08-29 15:36:17 +0900

    feat: SEO - MeatData

--- donghwan/my-app/src/app/products/[slug]/page.tsx
+++ donghwan/my-app/src/app/products/[slug]/page.tsx
@@ -4,6 +4,12 @@ type Props = {
   };
 };
 
+export function generateMetadata({ params }: Props) {
+  return {
+    title: `제품의 이름: ${params.slug}`,
+  };
+}
+
 export default function Pants({ params }: Props) {
   return <h1>{params.slug} 소개 페이지입니다!</h1>;
 }
