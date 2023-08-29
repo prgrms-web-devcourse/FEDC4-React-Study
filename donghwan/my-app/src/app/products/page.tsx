658c1dbdb9d0177ae5eef79f0c60d59f7a4b345e
Dongja <jdh981118@naver.com>
2023-08-29 15:14:25 +0900

    feat: Link

--- donghwan/my-app/src/app/products/page.tsx
+++ donghwan/my-app/src/app/products/page.tsx
@@ -1,3 +1,18 @@
-export default function Products() {
-  return <h1>제품 소개 페이지입니다!</h1>;
+import Link from 'next/link';
+
+const products = ['pants', 'shirt', 'skirt', 'shoes'];
+
+export default function ProductsLayout() {
+  return (
+    <>
+      <h1>제품 소개 페이지</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product}>
+            <Link href={`/products/${product}`}>{product}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
