fd9a4589fb435d85b19defa170cb9c306b7f370e
Dongja <jdh981118@naver.com>
2023-08-29 18:05:33 +0900

    feat: Server component

--- donghwan/my-app/src/app/page.tsx
+++ donghwan/my-app/src/app/page.tsx
@@ -1,5 +1,7 @@
-import Image from 'next/image';
+import os from 'os'; //node.js api
 
 export default function Home() {
+  console.log('안녕');
+  console.log(os.hostname());
   return <h1>홈페이지 입니다!</h1>;
 }
