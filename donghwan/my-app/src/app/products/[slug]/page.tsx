9abc985a0f8322bdd6a59fbe31ad1f60d8329932
Dongja <jdh981118@naver.com>
2023-08-30 14:53:23 +0900

    feat: 서버에서 데이터 받아서 ssg

--- donghwan/my-app/src/app/products/[slug]/page.tsx
+++ donghwan/my-app/src/app/products/[slug]/page.tsx
@@ -1,3 +1,6 @@
+import { getProduct, getProducts } from '@/service/products';
+import { notFound } from 'next/navigation';
+
 type Props = {
   params: {
     slug: string;
@@ -10,13 +13,20 @@ export function generateMetadata({ params }: Props) {
   };
 }
 
-export default function Pants({ params }: Props) {
-  return <h1>{params.slug} 소개 페이지입니다!</h1>;
+export default async function Product({ params: { slug } }: Props) {
+  // 서버에서 해당 데이터중 해당 제품의 정보를 찾아 그걸 보여줌
+  const product = await getProduct(slug);
+  if (!product) {
+    notFound();
+  }
+
+  return <h1>{product.name} 소개 페이지입니다!</h1>;
 }
 
-export function generateStaticParams() {
-  const products = ['pants', 'shirt'];
+export async function generateStaticParams() {
+  // 모든 제품의 페이지를 미리 만들거임 (SSG)
+  const products = await getProducts();
   return products.map((product) => ({
-    slug: product,
+    slug: product.id,
   }));
 }
